4dc471fc1265c5dfd6923d5602832a26436fc052
Sandro Padin <sandropadin@gmail.com>
2016-12-14 21:45:04 -0800

    Pass size and flatten preference to preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,21 +1,34 @@
 from driving_log.extract_features import ExtractFeatures
+from scipy.misc import imresize
 import numpy as np
 
-def normalize_feature(feature):
+def normalize_feature(feature, size, flatten):
     feature = np.array(feature)
+    feature = imresize(feature, size)
     feature = feature.astype(np.float32)
     feature /= 255.
     feature -= 0.5
+
+    if flatten == True:
+        feature = feature.reshape(-1, np.prod(np.shape(feature)))
+    else:
+        feature = feature.reshape(-1, *np.shape(feature))
+
     return feature
 
+def preprocess(datadir, filename, size, flatten):
+    while True:
+        datas = ExtractFeatures(datadir, filename).execute()
 
-def preprocess(datadir, filename):
-    datas = ExtractFeatures(datadir, filename).execute()
-
-    for feature, label in datas:
-        yield (normalize_feature(feature), label)
+        for feature, label in datas:
+            yield (normalize_feature(feature, size, flatten), np.array([label]))
 
 if __name__ == "__main__":
-    train = preprocess(datadir="data", filename="train_driving_log.csv")
-    valid = preprocess(datadir="data", filename="valid_driving_log.csv")
-    test = preprocess(datadir="data", filename="test_driving_log.csv")
+    size = (16, 32)
+    flatten = False
+    train = preprocess(datadir="data", filename="train_driving_log.csv", size=size, flatten=flatten)
+    valid = preprocess(datadir="data", filename="valid_driving_log.csv", size=size, flatten=flatten)
+    test = preprocess(datadir="data", filename="test_driving_log.csv", size=size, flatten=flatten)
+
+    first, _ = next(train)
+    print(first.shape)
