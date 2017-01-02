9db1ccf1cd070b900a2cc8be80c77f4734ba155f
Sandro Padin <sandropadin@gmail.com>
2017-01-02 23:07:46 +0000

    Skip header in read.py

--- read.py
+++ read.py
@@ -4,7 +4,7 @@ import sys
 def read(filepath):
     with open(filepath) as csv_file:
         csvreader = csv.reader(csv_file, skipinitialspace=True)
-        # next(csvreader) # skip header row
+        next(csvreader) # skip header row
         return [(row[0], row[1], row[2], row[3]) for row in csvreader]
 
 if __name__ == "__main__":
