34ba922bbf44dfd334394f239a5192ad71780c46
Sandro Padin <sandropadin@gmail.com>
2017-01-03 07:30:23 -0800

    Adding docstring to split.py

--- split.py
+++ split.py
@@ -1,6 +1,11 @@
 import math
 
 def row_slices(rows, pct_train, pct_valid, pct_test):
+    """
+    Returns a three slices that can be used to split the row data into
+    training, validation and test data. There is no overlap in the slices, so
+    validation and test data will not be in the training data.
+    """
     nb_rows = len(rows)
 
     split_1 = math.floor(nb_rows * pct_train)
@@ -12,6 +17,13 @@ def row_slices(rows, pct_train, pct_valid, pct_test):
             slice(split_2, split_3))
 
 def split(data, pct_train, pct_valid, pct_test):
+    """
+    Returns data split into three parts, each part representing training,
+    validation and test data repectively.
+
+    Note: pct_train + pct_valid + pct_test must equal 1. And each should be
+    between 0.0 and 1.0.
+    """
     train_slice, valid_slice, test_slice = row_slices(rows, pct_train, pct_valid, pct_test)
 
     return (rows[train_slice],
