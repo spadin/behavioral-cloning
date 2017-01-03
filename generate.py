4715b2e35b3adf8b50e373701bd924de53aa1bc9
Sandro Padin <sandropadin@gmail.com>
2017-01-03 07:18:25 -0800

    Include prepare_feature and prepare_label in train.py

--- generate.py
+++ generate.py
@@ -1,8 +1,15 @@
+from scipy.misc import imread
 from shuffle import shuffle
-from prepare import prepare_feature, prepare_label
 from split import split
 import csv
+import numpy as np
 import sys
+
+def prepare_feature(image):
+    return np.array([imread("{}/{}".format("data", image))]).astype(np.float32)
+
+def prepare_label(steering):
+    return np.array([float(steering)])
 
 def read(filepath):
     with open(filepath) as csv_file:
--- prepare.py
+++
@@ -1,8 +0,0 @@
-from scipy.misc import imread
-import numpy as np
-
-def prepare_feature(image):
-    return np.array([imread("{}/{}".format("data", image))]).astype(np.float32)
-
-def prepare_label(steering):
-    return np.array([float(steering)])
