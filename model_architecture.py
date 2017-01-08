a6b10d3e13bc20035ee5529320714f07d8102652
Sandro Padin <sandropadin@gmail.com>
2017-01-08 11:30:26 -0800

    Change filter size

--- model_architecture.py
+++ model_architecture.py
@@ -1,7 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Convolution2D, Dense, Flatten, Lambda
 from keras.layers.pooling import MaxPooling2D
-from keras.optimizers import Adam
 from keras.layers.advanced_activations import LeakyReLU
 
 def crop(x):
@@ -16,18 +15,18 @@ def model_architecture():
     model.add(Lambda(crop, input_shape=(160, 320, 3), name="crop"))
     model.add(Lambda(normalize, name="normalize"))
 
-    model.add(Convolution2D(10, 2, 2, subsample=(2, 2), border_mode="valid"))
+    model.add(Convolution2D(32, 2, 2, subsample=(2, 2), border_mode="same"))
     model.add(LeakyReLU())
 
-    model.add(Convolution2D(10, 2, 2, subsample=(2, 2), border_mode="valid"))
+    model.add(Convolution2D(64, 2, 2, subsample=(2, 2), border_mode="same"))
     model.add(LeakyReLU())
 
     model.add(MaxPooling2D())
 
-    model.add(Convolution2D(10, 2, 2, subsample=(2, 2), border_mode="valid"))
+    model.add(Convolution2D(32, 2, 2, subsample=(2, 2), border_mode="same"))
     model.add(LeakyReLU())
 
-    model.add(Convolution2D(10, 2, 2, subsample=(2, 2), border_mode="valid"))
+    model.add(Convolution2D(64, 2, 2, subsample=(2, 2), border_mode="same"))
     model.add(LeakyReLU())
 
     model.add(MaxPooling2D())
@@ -43,3 +42,7 @@ def model_architecture():
     model.add(Dense(1))
 
     return model
+
+if __name__ == '__main__':
+    model = model_architecture()
+    model.summary()
